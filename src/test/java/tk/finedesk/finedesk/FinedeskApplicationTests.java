576105007769181d418d38f07e01ba38440fec6b
asukiasyan@universe.dart.spb <anna.sukiasyan@dataart.com>
2022-01-03 12:59:41 +0400

    feature: [FD-2] Create entities and appropriate repositories

---
+++ src/test/java/tk/finedesk/finedesk/FinedeskApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.finedesk.finedesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinedeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
